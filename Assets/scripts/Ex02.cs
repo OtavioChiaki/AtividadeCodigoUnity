590b6a10a485351a67a1239f0781b2a85d91a321
OtavioChiaki <bovolonotavio@gmail.com>
2024-11-05 15:11:04 -0300

    Ex02

---
+++ Assets/scripts/Ex02.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ex02 : MonoBehaviour
+{ //02 - (Detecção de power-up) Ao coletar um power-up, o
+    //personagem aumenta sua velocidade. Se o jogador encontrar um
+    //power-up, exiba "Power-up Coletado". Caso contrário, exiba
+    //"Nenhum power-up encontrado".
+
+    [SerializeField] bool powerUp;//true
+    // ! -> Invertendo o valor 
+
+    
+    void Start()
+    {
+        if (powerUp == true)
+        {
+            print("Power-up Coletado!");
+        }
+        else
+        {
+            print("Nenhum power-up encontrado");
+        }
+
+        //condição ? Caso vdd : Caso falso 
+
+        print(powerUp ? "Power-up Coletado!" : "Nenhum power-up encontrado");
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
