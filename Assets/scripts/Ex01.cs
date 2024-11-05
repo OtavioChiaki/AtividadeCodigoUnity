f5bf8a9560d93c26df0ec6273f0a23271ec15e74
OtavioChiaki <bovolonotavio@gmail.com>
2024-11-05 15:08:44 -0300

    EX1

---
+++ Assets/scripts/Ex01.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ex01 : MonoBehaviour
+{  //01 - (Verificação de pontos de vida) Um personagem perde pontos
+    //de vida após uma batalha. Verifique se ele ainda está vivo. Caso
+    //tenha mais de 0 pontos de vida, exiba "Personagem Vivo", caso
+    //contrário, "Game Over".
+
+
+    [SerializeField] int vidaPlayer = 100;
+    string resultado;
+    
+    void Start()
+    {
+        if (vidaPlayer > 0)
+        {
+            print("Personagem vivo! :)");
+        }
+        else
+        {
+            print("Game Over");
+        }
+
+        // codinção ? valor a ser chamado caso verdadeiro : caso falso
+        resultado = (vidaPlayer > 0) ? "Personagem Vivo!" : "Game Over! ;-;";
+        print(resultado);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
