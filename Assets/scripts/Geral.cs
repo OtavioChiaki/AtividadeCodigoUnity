06d9b95586c045fc928e64d9951b1b26a86e2004
OtavioChiaki <bovolonotavio@gmail.com>
2024-11-05 15:04:52 -0300

    teste

---
+++ Assets/scripts/Geral.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Geral : MonoBehaviour
+{
+
+    //public string VariavelPublica;
+
+    //int vidaJogador = 10; //numero inteiro
+
+    //float velocidadeCarro = 125.5f; // numero quebrado
+
+    //char apelido = 'T'; // server para 1 caracter tipo o T que é uma letra 
+
+    //bool vivo = true; // serve para apenas 2 coisas verdade e falso
+
+    //string nomedojogador = "tadola"; // texto ou falando melhor mais de 1 caracter
+
+    //int vida_jogador;
+    // Start is called before the first frame update
+
+    //int variavelGlobal;
+
+    void Start()
+    {
+        //int variavelLocal = 10;
+        //print(variavelLocal);
+        //print(variavelGlobal);
+
+        //Debug.Log("Hello Word!");
+        //print(vivo);
+        //print(velocidadeCarro);
+        //print(VariavelPublica);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
