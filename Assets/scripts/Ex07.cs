53b74dad31359a34ea5326c6f9156655ca0b7bb7
OtavioChiaki <bovolonotavio@gmail.com>
2024-11-06 17:01:28 -0300

    Ex07

---
+++ Assets/scripts/Ex07.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ex07 : MonoBehaviour
+{
+    //7. (Dado com N faces) Para fazer um jogo de RPG, vamos precisar
+    //calcular probabilidades e simular a rolagem de dados.Crie um
+    //script em que o usuário defina o número de faces de um dado
+    //(int) e calcule a rolagem de um dado com esse número de faces.
+    //Use a função abaixo para o cálculo: Random.Range (valor_min,
+    //valor_max); (Coloque o código dentro da função-evento Start).
+
+    [SerializeField] int faces;
+
+    void Start()
+    {
+
+        //if (faces == 0)
+        //{
+        //    print("O número de faces do dado não pode ser: " + faces);
+        //}
+        //else
+        //{
+        //    print(Random.Range(1, faces));
+        //}
+
+        //condição ? vdd : falso
+
+        print(faces == 0 ? ("O número de faces do dado não pode ser: " + faces) : (Random.Range(1, faces)));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
