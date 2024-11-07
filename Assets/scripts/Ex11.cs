2c59bd08b617ce24fbe61f7605c3d95b796d0e59
OtavioChiaki <bovolonotavio@gmail.com>
2024-11-07 16:20:12 -0300

    Feat: Todos os 11 ex feitos

--- Assets/scripts/Ex11.cs
+++ Assets/scripts/Ex11.cs
@@ -4,10 +4,42 @@ using UnityEngine;
 
 public class Ex11 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //11. (Diálogos) Implemente um sistema de diálogos onde o jogador
+    //pode interagir com um NPC(personagem não jogável), e o NPC
+    //responde com diferentes frases dependendo do estado do jogo.
+    //Use switch case para definir as respostas baseadas no estado do
+    //jogador.
+
+    [SerializeField] int estado;
     void Start()
     {
-        
+
+        switch (estado)
+        {
+            case 1:
+                print("Da para fazer com ternario?");
+                break;
+
+            case 2:
+                print("Sextooou!");
+                break;
+
+            case 3:
+                print("Cadê o Thiago?");
+                break;
+
+            case 4:
+                print("Cadê a Alana?");
+                break;
+
+            case 5:
+                print("Jucelino, cadê meu aumento?");
+                break;
+
+            default:
+                print("Não tenho nada a dizer sobre isso!");
+                break;
+        }
     }
 
     // Update is called once per frame
