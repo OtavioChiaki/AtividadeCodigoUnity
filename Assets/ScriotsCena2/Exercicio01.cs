1bcddbc1a365e9459d25add65b0f149776a01451
OtavioChiaki <bovolonotavio@gmail.com>
2024-11-11 15:23:11 -0300

    Feat: atividade de repetir 01

---
+++ Assets/ScriotsCena2/Exercicio01.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exercicio01 : MonoBehaviour
+{
+    int x = 0; //While
+    int y = 10; //DoWhile
+
+    List<int> nums = new List<int>();
+    void Start()
+    {
+        //for (inicializador; condição; incremento / decremento)
+            for (int i = 5; i >= 1; i--)
+            {
+
+                print(i);
+              
+            }
+        print("Início da partida!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/scripts/LacoRepeticao.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LacoRepeticao : MonoBehaviour
+{
+    // for é usado quando sabe a quantidade de vezes que precisa repetir
+    //while ou dowhile é utilizado quando não sabe a quantidade de vezes a se repetir
+    // Foreach usado em listas de arrays para executar uma operação
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
