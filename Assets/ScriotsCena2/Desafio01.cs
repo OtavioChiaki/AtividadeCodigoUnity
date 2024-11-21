6db92f3aba8bc78244155991fc5b22c4cb5b3601
OtavioChiaki <bovolonotavio@gmail.com>
2024-11-21 14:56:51 -0300

    Feat: Desafio01

---
+++ Assets/ScriotsCena2/Desafio01.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Desafio01 : MonoBehaviour
+{
+    //Escreva um programa que conta o número de consoantes em uma string.
+
+    [SerializeField] string texto = "JOGOS digitais";
+    [SerializeField] int contadorConsoantes;
+
+    void Start()
+    {
+        //for(inicializador; condição; incremento ou decremento)
+        for (int i = 0; i < texto.Length; i++)
+        {
+            char letra = texto[i];
+
+            if ("bcdfghjklmnpqrstvwxyz".Contains(char.ToLower(letra)))
+            {
+                contadorConsoantes++;
+            }
+
+            //(Errado) 
+            //if (!"aeiou".Contains(char.ToLower(letra)))
+            //{
+            //    contadorConsoantes++;
+            //}
+        }
+        print("O número de consoantes é: " + contadorConsoantes);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
